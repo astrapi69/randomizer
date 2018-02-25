ed6bd968a8a2bff784c19776ea4cc75605097ae1
Asterios Raptis <asterios.raptis@gmx.net>
2018-02-25 09:53:48 +0100

    unit test fixed

--- randomizer-core/src/main/java/de/alpharogroup/random/RandomObjectsExtensions.java
+++ randomizer-core/src/main/java/de/alpharogroup/random/RandomObjectsExtensions.java
@@ -28,10 +28,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * The class {@link RandomObjectsExtensions} is a utility class to create random objects.
  */
-public class RandomObjectsExtensions
+@UtilityClass
+public final class RandomObjectsExtensions
 {
 
 	/**
