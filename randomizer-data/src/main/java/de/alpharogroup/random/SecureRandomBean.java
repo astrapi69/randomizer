1c062b950d2f728b60921f647b1d5bfbf0a12374
astrapi69 <asterios.raptis@gmx.net>
2020-02-17 15:04:26 +0100

    added new seed field to the SecureRandomBean class for init with a Date object

--- randomizer-data/src/main/java/de/alpharogroup/random/SecureRandomBean.java
+++ randomizer-data/src/main/java/de/alpharogroup/random/SecureRandomBean.java
@@ -32,16 +32,18 @@ import java.security.SecureRandom;
  */
 public final class SecureRandomBean
 {
+
 	public static class SecureRandomBeanBuilder
 	{
 		private String algorithm;
 		private String provider;
+		private long seed;
 
 		SecureRandomBeanBuilder()
 		{
 		}
 
-		public SecureRandomBeanBuilder algorithm(final String algorithm)
+		public SecureRandomBean.SecureRandomBeanBuilder algorithm(String algorithm)
 		{
 			this.algorithm = algorithm;
 			return this;
@@ -49,117 +51,99 @@ public final class SecureRandomBean
 
 		public SecureRandomBean build()
 		{
-			return new SecureRandomBean(algorithm, provider);
+			return new SecureRandomBean(algorithm, provider, seed);
 		}
 
-		public SecureRandomBeanBuilder provider(final String provider)
+		public SecureRandomBean.SecureRandomBeanBuilder provider(String provider)
 		{
 			this.provider = provider;
 			return this;
 		}
 
-		@Override
+		public SecureRandomBean.SecureRandomBeanBuilder seed(long seed)
+		{
+			this.seed = seed;
+			return this;
+		}
+
 		public String toString()
 		{
 			return "SecureRandomBean.SecureRandomBeanBuilder(algorithm=" + this.algorithm
-				+ ", provider=" + this.provider + ")";
+				+ ", provider=" + this.provider + ", seed=" + this.seed + ")";
 		}
 	}
-	/**
-	 * The Constant DEFAULT_ALGORITHM.
-	 */
+	/** The Constant DEFAULT_ALGORITHM. */
 	public static final String DEFAULT_ALGORITHM = "SHA1PRNG";
-	/**
-	 * The Constant DEFAULT_ALGORITHM.
-	 */
+
+	/** The Constant DEFAULT_ALGORITHM. */
 	public static final String DEFAULT_PROVIDER = "SUN";
+
 	public static SecureRandomBeanBuilder builder()
 	{
 		return new SecureRandomBeanBuilder();
 	}
 
-	/**
-	 * The algorithm.
-	 */
+	/** The algorithm. */
 	private final String algorithm;
 
-	/**
-	 * The provider.
-	 */
+	/** The provider. */
 	private final String provider;
+
+	/** The seed. */
+	private final long seed;
 
 	/**
 	 * Instantiates a new {@link SecureRandomBean} object with the default values
 	 */
 	public SecureRandomBean()
 	{
-		this(DEFAULT_ALGORITHM, DEFAULT_PROVIDER);
+		this(DEFAULT_ALGORITHM, DEFAULT_PROVIDER, System.currentTimeMillis());
 	}
 
-	/**
-	 * Instantiates a new {@link SecureRandomBean} with the given values
-	 *
-	 * @param defaultAlgorithm
-	 *            the default algorithm
-	 * @param defaultProvider
-	 *            the default provider
-	 */
-	public SecureRandomBean(String defaultAlgorithm, String defaultProvider)
+	public SecureRandomBean(String algorithm, String provider, long seed)
 	{
-		this.algorithm = defaultAlgorithm;
-		this.provider = defaultProvider;
+		this.algorithm = algorithm;
+		this.provider = provider;
+		this.seed = seed;
 	}
 
-	@Override
 	public boolean equals(final Object o)
 	{
 		if (o == this)
-		{
 			return true;
-		}
 		if (!(o instanceof SecureRandomBean))
-		{
 			return false;
-		}
 		final SecureRandomBean other = (SecureRandomBean)o;
 		final Object this$algorithm = this.getAlgorithm();
 		final Object other$algorithm = other.getAlgorithm();
 		if (this$algorithm == null
 			? other$algorithm != null
 			: !this$algorithm.equals(other$algorithm))
-		{
 			return false;
-		}
 		final Object this$provider = this.getProvider();
 		final Object other$provider = other.getProvider();
 		if (this$provider == null ? other$provider != null : !this$provider.equals(other$provider))
-		{
 			return false;
-		}
+		if (this.getSeed() != other.getSeed())
+			return false;
 		return true;
 	}
 
-	/**
-	 * The algorithm.
-	 *
-	 * @return the algorithm
-	 */
 	public String getAlgorithm()
 	{
 		return this.algorithm;
 	}
 
-	/**
-	 * The provider.
-	 *
-	 * @return the provider
-	 */
 	public String getProvider()
 	{
 		return this.provider;
 	}
 
-	@Override
+	public long getSeed()
+	{
+		return this.seed;
+	}
+
 	public int hashCode()
 	{
 		final int PRIME = 59;
@@ -168,18 +152,20 @@ public final class SecureRandomBean
 		result = result * PRIME + ($algorithm == null ? 43 : $algorithm.hashCode());
 		final Object $provider = this.getProvider();
 		result = result * PRIME + ($provider == null ? 43 : $provider.hashCode());
+		final long $seed = this.getSeed();
+		result = result * PRIME + (int)($seed >>> 32 ^ $seed);
 		return result;
 	}
 
 	public SecureRandomBeanBuilder toBuilder()
 	{
-		return new SecureRandomBeanBuilder().algorithm(this.algorithm).provider(this.provider);
+		return new SecureRandomBeanBuilder().algorithm(this.algorithm).provider(this.provider)
+			.seed(this.seed);
 	}
 
-	@Override
 	public String toString()
 	{
 		return "SecureRandomBean(algorithm=" + this.getAlgorithm() + ", provider="
-			+ this.getProvider() + ")";
+			+ this.getProvider() + ", seed=" + this.getSeed() + ")";
 	}
 }
