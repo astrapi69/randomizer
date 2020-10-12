997b4a90b2ed2eeed196cc821522928fd1c95e34
astrapi69 <asterios.raptis@gmx.net>
2020-10-12 16:54:48 +0200

    javadoc warnings fixed

--- randomizer-core/src/main/java/de/alpharogroup/random/number/RandomBigIntegerFactory.java
+++ randomizer-core/src/main/java/de/alpharogroup/random/number/RandomBigIntegerFactory.java
@@ -1,8 +1,8 @@
 /**
  * The MIT License
- * <p>
+ *
  * Copyright (C) 2015 Asterios Raptis
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -52,6 +52,8 @@ public final class RandomBigIntegerFactory {
 	/**
 	 * Generates a random {@link BigInteger}
 	 *
+	 * @param secureRandom
+	 *            the secure random for generation
 	 * @return the random {@link BigInteger}
 	 */
 	public static BigInteger randomBigInteger(SecureRandom secureRandom)
@@ -72,6 +74,8 @@ public final class RandomBigIntegerFactory {
 	/**
 	 * Returns a random serial number that can be used for a serial number.
 	 *
+	 * @param secureRandom
+	 *            the secure random for generation
 	 * @return a random serial number as a {@link BigInteger} object.
 	 */
 	public static BigInteger randomSerialNumber(SecureRandom secureRandom)
--- randomizer-core/src/main/java/de/alpharogroup/random/number/RandomIntFactory.java
+++ randomizer-core/src/main/java/de/alpharogroup/random/number/RandomIntFactory.java
@@ -1,8 +1,8 @@
 /**
  * The MIT License
- * <p>
+ *
  * Copyright (C) 2015 Asterios Raptis
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -10,10 +10,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -160,6 +160,8 @@ public final class RandomIntFactory {
 	 *            if true than min value is included
 	 * @param includeMax
 	 *            if true than max value is included
+	 * @param secureRandom
+	 *            the secure random for generation
 	 * @return A random int between the range from start and end.
 	 */
 	public static int randomIntBetween(final int start, final int end, final boolean includeMin,
